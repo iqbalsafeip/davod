0e4018272747cfc954e5e5e3388a6ff5ab700a27
iqbalsafeip <isaacfrost9841@gmail.com>
2025-09-09 12:41:18 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -5,24 +5,7 @@ export default function Home() {
   const InfoCard = (
     <GuideInfoBox>
       <ul>
-        <li className="text-l">
-          🤝
-          <span className="ml-2">
-            This template showcases a simple chatbot using{" "}
-            <a href="https://js.langchain.com/" target="_blank">
-              LangChain.js
-            </a>{" "}
-            and the Vercel{" "}
-            <a href="https://sdk.vercel.ai/docs" target="_blank">
-              AI SDK
-            </a>{" "}
-            in a{" "}
-            <a href="https://nextjs.org/" target="_blank">
-              Next.js
-            </a>{" "}
-            project.
-          </span>
-        </li>
+       
       
         <li>
           🏴‍☠️
